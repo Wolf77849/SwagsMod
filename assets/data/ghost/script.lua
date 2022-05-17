ec63a3375addad8709e30c174c9197c740bc8f8c
Swagwhisperer10 <96598000+Swagwhisperer10@users.noreply.github.com>
2022-05-17 15:14:02 -0500

    Add files via upload

---
+++ assets/data/ghost/script.lua
@@ -0,0 +1,8 @@
+function opponentNoteHit()
+        if difficulty >= 2 then
+            health = getProperty('health')
+            if getProperty('health') > 0.3 then
+                setProperty('health', health- 0.014);
+            end
+        end
+end
