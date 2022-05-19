e391b9f4f17148e128b234dfca6d78f4c97823b9
Swagwhisperer10 <96598000+Swagwhisperer10@users.noreply.github.com>
2022-05-19 17:09:22 -0500

    Delete assets/data directory

--- assets/data/ghost/script.lua
+++
@@ -1,8 +0,0 @@
-function opponentNoteHit()
-        if difficulty >= 2 then
-            health = getProperty('health')
-            if getProperty('health') > 0.3 then
-                setProperty('health', health- 0.014);
-            end
-        end
-end
