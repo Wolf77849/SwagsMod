c4470138cc04fc1f8fbba411fc33f4f22faca0eb
Swagwhisperer10 <96598000+Swagwhisperer10@users.noreply.github.com>
2022-07-30 20:33:03 -0500

    8:32 PM
    Added Rush E

--- mods/data/limitless/script.lua
+++ mods/data/limitless/script.lua
@@ -1,3 +1,20 @@
+local angleshit = 1;
+local anglevar = 1;
+function onBeatHit()
+    if curBeat >= 64 and curBeat <= 704 then
+        if curBeat % 2 == 0 then
+            angleshit = anglevar;
+        else
+            angleshit = -anglevar;
+        end
+        setProperty('camHUD.angle',angleshit*3)
+        setProperty('camGame.angle',angleshit*3)
+        doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+        doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+    end
+end
 function onUpdate()
     if curStep == 734 then -- First time
         setPropertyFromClass('ClientPrefs', 'camZooms', false);
