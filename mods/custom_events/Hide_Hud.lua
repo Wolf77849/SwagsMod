817e0415aa05389eb039acd224ee99f0c0736bc3
Swagwhisperer10 <107.auri@gmail.com>
2023-02-11 01:57:58 -0600

    Release 2.4
    Psych Engine 0.6.3 (The game says 0.6.2 idk why)
    Added videos to yonkagor song and sonic wave song.
    Changed some other events kek

--- mods/custom_events/Hide_Hud.lua
+++ mods/custom_events/Hide_Hud.lua
@@ -8,6 +8,10 @@ function onEvent(name,value1,value2)
 			setProperty('scoreTxt.alpha', 1);
 			setProperty('timeBar.alpha', 1);
 			setProperty('timeTxt.alpha', 1);
+			noteTweenAlpha("o5",4,1,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,1,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,1,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,1,0.5,"quartInOut");
 			doTweenAlpha('GUItween', 'camHUD', 1, 0.5, 'linear');
 		end
 		if value2 == '0' then
@@ -18,14 +22,38 @@ function onEvent(name,value1,value2)
 			setProperty('scoreTxt.alpha', 1);
 			setProperty('timeBar.alpha', 1);
 			setProperty('timeTxt.alpha', 1);
+			noteTweenAlpha("o5",4,1,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,1,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,1,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,1,0.5,"quartInOut");
 			doTweenAlpha('GUItween', 'camHUD', 1, 0.5, 'linear');
 		end
 		if value1 == '1' then
-			doTweenAlpha('GUItween', 'camHUD', 0, 0.5, 'linear');
+			setProperty('healthBar.alpha', 0);
+			setProperty('healthBarBG.alpha', 0);
+			setProperty('iconP1.alpha', 0);
+			setProperty('iconP2.alpha', 0);
+			setProperty('scoreTxt.alpha', 0);
+			setProperty('timeBar.alpha', 0);
+			setProperty('timeTxt.alpha', 0);
+			noteTweenAlpha("o5",4,0,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,0,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,0,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,0,0.5,"quartInOut");
 		end
 	
 		if value1 == '2' then
-			doTweenAlpha('GUItween', 'camHUD', 1, 0.5, 'linear');
+			setProperty('healthBar.alpha', 1);
+			setProperty('healthBarBG.alpha', 1);
+			setProperty('iconP1.alpha', 1);
+			setProperty('iconP2.alpha', 1);
+			setProperty('scoreTxt.alpha', 1);
+			setProperty('timeBar.alpha', 1);
+			setProperty('timeTxt.alpha', 1);
+			noteTweenAlpha("o5",4,1,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,1,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,1,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,1,0.5,"quartInOut");
 		end
 		if value2 == '1' then
 			setProperty('healthBar.alpha', 0);
@@ -35,6 +63,10 @@ function onEvent(name,value1,value2)
 			setProperty('scoreTxt.alpha', 0);
 			setProperty('timeBar.alpha', 0);
 			setProperty('timeTxt.alpha', 0);
+			noteTweenAlpha("o5",4,0,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,0,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,0,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,0,0.5,"quartInOut");
 		end
 		if value2 == '2' then
 			setProperty('healthBar.alpha', 1);
@@ -44,6 +76,10 @@ function onEvent(name,value1,value2)
 			setProperty('scoreTxt.alpha', 1);
 			setProperty('timeBar.alpha', 1);
 			setProperty('timeTxt.alpha', 1);
+			noteTweenAlpha("o5",4,1,0.5,"quartInOut");
+            noteTweenAlpha("o6",5,1,0.5,"quartInOut");
+            noteTweenAlpha("o7",6,1,0.5,"quartInOut");
+            noteTweenAlpha("o8",7,1,0.5,"quartInOut");
 		end
 	end	
 end
---
+++ mods/custom_events/mp4bg.lua
@@ -0,0 +1,3 @@
+function onEvent()
+    callScript('scripts/mp4thingy', 'makeVideoSprite', {'video1', value1, 0, 0, 'camHUD'})
+end
